5540d7008f674113cad6abf44643ce69f0741fcc
Dreng0t <louischicot4@gmail.com>
2025-04-04 17:14:33 +0100

    Lab solved

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,19 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add("1", "1")).toEqual(undefined);
+        expect(add("1", 1)).toEqual(undefined);
+        expect(add(1, "1")).toEqual(undefined);
+      });
     });
   });
-  
+
+  /*
+  1. There are 8 tests.
+  2. Describe and It are being used to organise and write the tests, describe nreaks the test suite into components and it is for individual tests.
+  3. The test descriptions are phrased so that a human reading them can understand what the test is for, the word should stands out as they describe what should happen to pass the test.
+  4. The expect functions describe a potential scenario that the tested function might encounter and the outcome we would expect to occur if the function passes the test successfully.
+     The expect function take in the tested function with some arguments as input, the output of which is compared to the toEqual function to determine if the test was a success.
+  */
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,35 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+          });
+      
+          it("should take two arguments", () => {
+            expect(divide.length).toBe(2);
+          });
+      
+          it("should return the division of the two numbers", () => {
+            expect(divide(2, 1)).toEqual(2);
+            expect(divide(3, 4)).toEqual(0.75);
+            expect(divide(15, 5)).toEqual(3);
+          });
+      
+          it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(1)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 1)).toEqual(undefined);
+          });
+    
+          it("should return undefined if any of the two arguments is not a number", () => {
+            expect(divide("1", "1")).toEqual(undefined);
+            expect(divide("1", 1)).toEqual(undefined);
+            expect(divide(1, "1")).toEqual(undefined);
+          });
 
     })    
 })
 
-*/
